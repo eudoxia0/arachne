c941812f4ab19ee94c2e6219b4567fabd0df24d3
Fernando Borretti <eudoxiahp@gmail.com>
2013-06-15 10:32:23 -0300

    Make seen-urls a hash table

--- src/url.lisp
+++ src/url.lisp
@@ -1,8 +1,23 @@
 (in-package #:arachne)
 
+(defclass seen-urls ()
+    ((urls
+        :accessor   urls
+        :initarg    :urls
+        :initform   (make-hash-table))))
+
+(defmethod seenp ((seen seen-urls) request)
+    (gethash (url request) (urls seen)))
+
 (defun normalize (url &optional (base nil))
     "Performs URL normalization, optionally with a base URL to use with relative
-    URLs".)
+    URLs"
+    url)
 
-(defun seenp (request seen-urls)
-    (member request seen-urls :test #'equal))
+(defmethod valid (request seen-urls base-url)
+    "Checks if a URL is valid:
+        1. Normalizes it
+        2. Tests it against the seen-urls:
+            * If it's seen, returns nil
+            * If it's not, returns t, and adds the url to `seen-urls`"
+    (seenp seen-urls (normalize (url request) base-url)))
