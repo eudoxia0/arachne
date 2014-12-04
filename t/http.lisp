b1422d7387736d8d8c9f540b8b1fe9ff34e14a6b
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-04 19:57:40 -0200

    Add some http tests

--- t/http.lisp
+++ t/http.lisp
@@ -7,6 +7,15 @@
   (make-instance 'arachne.http:<request>
                  :url (arachne-test.server:make-url "/test")))
 
+(defparameter +not-found-request+
+  (make-instance 'arachne.http:<request>
+                 :url (arachne-test.server:make-url "/some-wrong-url")))
+
+(defparameter +metadata-request+
+  (make-instance 'arachne.http:<request>
+                 :url (arachne-test.server:make-url "/test")
+                 :metadata (list (cons "Test" 'data))))
+
 (defvar *response*)
 
 (test simple-request
@@ -24,3 +33,28 @@
    (equal
     (arachne.http:response-body *response*)
     "Test Response")))
+
+(test not-found-request
+  (finishes
+    (setf *response* (arachne.http:send +not-found-request+)))
+  (is-true
+   (typep
+    *response*
+    'arachne.http:<response>))
+  (is
+   (equal
+    (arachne.http:response-status *response*)
+    404)))
+
+(test metadata-request
+  (finishes
+    (setf *response* (arachne.http:send +metadata-request+)))
+  (is-true
+   (typep
+    *response*
+    'arachne.http:<response>)))
+
+(test clean-up
+  (finishes
+   ;; For future tests
+   (setf *response* nil)))
