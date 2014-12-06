1739d8f6e448ca8e28a0726ed4df781bf1b14ab1
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-06 00:12:31 -0200

    Add some more package definitions

--- src/packages.lisp
+++ src/packages.lisp
@@ -29,3 +29,39 @@
            :cookie-http-only-p)
   (:documentation "Implements requests and responses and the machinery to send
  and receive them through Drakma."))
+
+(defpackage :arachne.downloader
+  (:use :cl :trivial-types)
+  (:export :<middleware>
+           :process-request
+           :process-response
+           :<downloader>
+           :download)
+  (:documentation "The Downloader downloads takes requests, passes them through
+request middlewares (Which can modify, or drop those requests), downloads them
+into responses, sends the response through the chain of middlewares, and finally
+returns the processed response."))
+
+(defpackage :arachne.items
+  (:use :cl :trivial-types)
+  (:export :<item>
+           :copy
+           :<filter>
+           :start
+           :stop
+           :filter
+           :<pipeline>
+           :start-pipeline
+           :stop-pipeline
+           :filter))
+
+(defpackage :arachne.worker
+  (:use :cl :trivial-types)
+  (:export :<worker>))
+
+(defpackage :arachne.spiders
+  (:use :cl :trivial-types))
+
+(defpackage :arachne.utils
+  (:use :cl)
+  (:export :hash))
