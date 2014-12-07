b26c1566d7345bb4feba9843394a6c3b93cbfc1b
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-07 17:46:03 -0200

    Add some docstrings

--- src/worker.lisp
+++ src/worker.lisp
@@ -11,10 +11,15 @@
              :documentation "The worker's item pipeline.")))
 
 (defmethod start ((worker <worker>))
+  "Start the worker."
   (arachne.items:start-pipeline (pipeline worker)))
 
 (defmethod stop ((worker <worker>))
+  "Stop the worker."
   (arachne.items:stop-pipeline (pipeline worker)))
 
 (defmethod filter ((worker <worker>) (item arachne.items:<item>))
-  (arachne.items:filter (pipeline worker) item))
+  "Filter an item through the worker's pipeline. Returns nil if it was rejected,
+t otherwise."
+  (when (arachne.items:filter (pipeline worker) item)
+    t))
