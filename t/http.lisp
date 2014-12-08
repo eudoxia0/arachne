ba0d5b9b36d98208e0c0379a2afccd8f6904d7ab
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-08 09:43:18 -0200

    Move some global constants to local variables

--- t/http.lisp
+++ t/http.lisp
@@ -3,74 +3,71 @@
 (def-suite http)
 (in-suite http)
 
-(defparameter +simple-request+
-  (make-instance 'arachne.http:<request>
-                 :url (arachne-test.server:make-url "/test")))
-
-(defparameter +not-found-request+
-  (make-instance 'arachne.http:<request>
-                 :url (arachne-test.server:make-url "/some-wrong-url")))
-
-(defparameter +metadata-request+
-  (make-instance 'arachne.http:<request>
-                 :url (arachne-test.server:make-url "/test")
-                 :metadata (arachne.utils:hash
-                            (1 2)
-                            ("Test" "data"))))
-
 (defvar *response*)
 
 (test simple-request
-  (finishes
-    (setf *response* (arachne.http:send +simple-request+)))
-  (is-true
-   (typep
-    *response*
-    'arachne.http:<response>))
-  (is
-   (equal
-    (arachne.http:response-status *response*)
-    201))
-  (is
-   (equal
-    (arachne.http:response-body *response*)
-    "Test Response")))
+  (let ((simple-request
+        (make-instance 'arachne.http:<request>
+                       :url (arachne-test.server:make-url "/test"))))
+    (finishes
+      (setf *response* (arachne.http:send simple-request)))
+    (is-true
+     (typep
+      *response*
+      'arachne.http:<response>))
+    (is
+     (equal
+      (arachne.http:response-status *response*)
+      201))
+    (is
+     (equal
+      (arachne.http:response-body *response*)
+      "Test Response"))))
 
 (test not-found-request
-  (finishes
-    (setf *response* (arachne.http:send +not-found-request+)))
-  (is-true
-   (typep
-    *response*
-    'arachne.http:<response>))
-  (is
-   (equal
-    (arachne.http:response-status *response*)
-    404)))
+  (let ((not-found-request
+          (make-instance 'arachne.http:<request>
+                         :url (arachne-test.server:make-url "/some-wrong-url"))))
+    (finishes
+      (setf *response* (arachne.http:send not-found-request)))
+    (is-true
+     (typep
+      *response*
+      'arachne.http:<response>))
+    (is
+     (equal
+      (arachne.http:response-status *response*)
+      404))))
 
 (test metadata-request
-  (finishes
-    (setf *response* (arachne.http:send +metadata-request+)))
-  (is-true
-   (typep
-    *response*
-    'arachne.http:<response>))
+  (let ((metadata-request
+          (make-instance 'arachne.http:<request>
+                         :url (arachne-test.server:make-url "/test")
+                         :metadata (arachne.utils:hash
+                                    (1 2)
+                                    ("Test" "data")))))
+    (finishes
+      (setf *response* (arachne.http:send metadata-request)))
+    (is-true
+     (typep
+      *response*
+      'arachne.http:<response>))
     (is
-   (equal
-    (arachne.http:response-status *response*)
-    201))
-  (is
-   (equal
-    (arachne.http:response-body *response*)
-    "Test Response"))
-  (is
-   (equal
-    (gethash 1 (arachne.http:response-metadata *response*))
-    2))
-  (is
-   (equal
-    (gethash "Test" (arachne.http:response-metadata *response*))
-    "data")))
+     (equal
+      (arachne.http:response-status *response*)
+      201))
+    (is
+     (equal
+      (arachne.http:response-body *response*)
+      "Test Response"))
+    (is
+     (equal
+      (gethash 1 (arachne.http:response-metadata *response*))
+      2))
+    (is
+     (equal
+      (gethash "Test" (arachne.http:response-metadata *response*))
+      "data"))))
 
 (test clean-up
   (finishes
