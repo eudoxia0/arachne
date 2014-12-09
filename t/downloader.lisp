8a37ff9dac0948cb03c7f64771957c30f5dd16f2
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-09 10:57:28 -0200

    Test that downloader middlewares work

--- t/downloader.lisp
+++ t/downloader.lisp
@@ -22,3 +22,30 @@
     (is
      (equal (arachne.http:response-body response)
             "Test Response"))))
+
+(defclass <test-middleware> (arachne.downloader:<middleware>) ())
+
+(defmethod arachne.downloader:process-request ((middleware <test-middleware>)
+                                               (request arachne.http:<request>))
+  (setf (gethash :request (arachne.http:request-metadata request)) t))
+
+(defmethod arachne.downloader:process-response ((middleware <test-middleware>)
+                                                (response arachne.http:<response>))
+  (setf (gethash :response (arachne.http:response-metadata response)) t))
+
+(test simple-download
+  (let ((downloader
+          (make-instance 'arachne.downloader:<downloader>
+                         :middlewares (list (make-instance '<test-middleware>))))
+        (simple-request
+          (make-instance 'arachne.http:<request>
+                         :url (arachne-test.server:make-url "/test")))
+        (response))
+    (finishes
+      (setf response (arachne.downloader:download downloader simple-request)))
+    (is-true
+     (typep response 'arachne.http:<response>))
+    (is-true
+     (gethash :request (arachne.http:response-metadata response)))
+    (is-true
+     (gethash :response (arachne.http:response-metadata response)))))
