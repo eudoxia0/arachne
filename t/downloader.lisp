f860015dcdb9d129e18dd3f267071275b75b010d
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-09 10:51:15 -0200

    Add some tests for the downloader

--- t/downloader.lisp
+++ t/downloader.lisp
@@ -2,3 +2,23 @@
 
 (def-suite downloader)
 (in-suite downloader)
+
+(defparameter +empty-downloader+
+  (make-instance 'arachne.downloader:<downloader>))
+
+(test simple-download
+  (let ((simple-request
+          (make-instance 'arachne.http:<request>
+                         :url (arachne-test.server:make-url "/test")))
+        (response))
+    (finishes
+      (setf response (arachne.downloader:download +empty-downloader+
+                                                  simple-request)))
+    (is-true
+     (typep response 'arachne.http:<response>))
+    (is
+     (equal (arachne.http:response-status response)
+            201))
+    (is
+     (equal (arachne.http:response-body response)
+            "Test Response"))))
