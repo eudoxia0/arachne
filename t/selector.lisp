539d55a610b8257bbf70c7715aa32004d9e9ed93
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-10 19:29:31 -0200

    Add some test data for the selector tests

--- t/selector.lisp
+++ t/selector.lisp
@@ -16,3 +16,29 @@
                          (make-instance 'arachne.http:<response>
                                         :body "<doc></doc>"))
           'arachne.selector:<selector>)))
+
+(defparameter +simple-document+ "
+<list>
+  <item>1</item>
+  <item class='a'>2</item>
+  <item class='a b'>3</item>
+</list>")
+
+(defparameter +booklist+ "
+<booklist>
+  <book>
+    <title>ANSI Common Lisp</title>
+    <author>Paul Graham</author>
+  </book>
+  <book>
+    <title>Practical Common Lisp</title>
+    <author>Peter Seibel</author>
+  </book>
+</booklist>")
+
+(test css
+  (let ((selector (make-instance 'arachne.selector:<selector>
+                                 :text +simple-document+)))
+    (finishes
+      (print (arachne.selector:css selector "list > item"))
+      (print (arachne.selector:xpath selector "//item[@class='a']")))))
